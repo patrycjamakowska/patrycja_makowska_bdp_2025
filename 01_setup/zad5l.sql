a3c5e9ca85d9957558f12b5cb734ef0d02dcff4a
patrycja <147620950+patrycjamakowska@users.noreply.github.com>
2025-10-13 18:57:12 +0200

    Update zad5l.sql

--- 01_setup/zad5l.sql
+++ 01_setup/zad5l.sql
@@ -2,3 +2,15 @@ SELECT AVG(CAST(pensja.kwota AS decimal(10, 2))) AS średnia
 FROM ksiegowosc.wynagrodzenie
 JOIN ksiegowosc.pensja ON pensja.id_pensji=wynagrodzenie.id_pensji
 WHERE pensja.stanowisko='sprzedawca'
+
+SELECT MIN(CAST(pensja.kwota AS decimal(10, 2))) AS minimalna,stanowisko
+FROM ksiegowosc.wynagrodzenie
+INNER JOIN ksiegowosc.pensja ON pensja.id_pensji=wynagrodzenie.id_pensji
+WHERE pensja.stanowisko='sprzedawca'
+GROUP BY pensja.stanowisko;
+
+SELECT MAX(CAST(pensja.kwota AS decimal(10, 2))) AS maksymalna,stanowisko
+FROM ksiegowosc.wynagrodzenie
+INNER JOIN ksiegowosc.pensja ON pensja.id_pensji=wynagrodzenie.id_pensji
+WHERE pensja.stanowisko='sprzedawca'
+GROUP BY pensja.stanowisko;
