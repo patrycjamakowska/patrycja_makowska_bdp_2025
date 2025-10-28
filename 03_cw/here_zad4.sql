3f53a7fdc3f3d4f2ae280438e4735a07095437e3
patrycja <147620950+patrycjamakowska@users.noreply.github.com>
2025-10-28 11:12:47 +0100

    Create here_zad4.sql

---
+++ 03_cw/here_zad4.sql
@@ -0,0 +1,18 @@
+--zadanie 4
+--tworzenie tabeli 
+DROP TABLE IF EXISTS input_points;
+
+CREATE TABLE input_points (
+id SERIAL,
+geom geometry(Point)
+);
+
+INSERT INTO input_points (geom)
+VALUES
+    (ST_SetSRID(ST_MakePoint(8.36093, 49.03174), 4326)),
+    (ST_SetSRID(ST_MakePoint(8.39876, 49.00644), 4326));
+
+--zmiana ukladu
+ALTER TABLE input_points
+ALTER COLUMN geom TYPE geometry(Point, 3068)
+USING ST_Transform(geom, 3068);
